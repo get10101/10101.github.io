154b18b02d043af8303d82d11bdcee2bcf408c06
Daniel Karzel <daniel@comit.network>
2023-05-16 14:55:39 -0400

    Configure docusaurus to reflect 10101

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: '10101 - Decentralised finance. For real.',
+  tagline: 'Decentralised finance. For real.',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'get10101', // Usually your GitHub org/user name.
+  projectName: '10101', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'github.com/get10101/10101.github.io/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'github.com/get10101/10101.github.io',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -64,10 +64,11 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '10101.finance',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: '10101 logo',
+          src: 'img/logo/logo.svg',
+          srcDark: 'img/logo/logo_dark-theme.svg',
         },
         items: [
           {
@@ -78,8 +79,18 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            html: 'Twitter',
+            href: 'https://twitter.com/get10101',
+            position: 'right',
+          },
+          {
+            html: 'Telegram',
+            href: 'https://t.me/get10101',
+            position: 'right',
+          },
+          {
+            html: 'GitHub',
+            href: 'https://github.com/get10101/10101',
             position: 'right',
           },
         ],
@@ -88,46 +99,27 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
+                label: 'Back to top ⬆️',
                 to: '/blog',
               },
               {
+                label: 'Twitter',
+                href: 'https://twitter.com/get10101',
+              },
+              {
+                label: 'Telegram',
+                href: 'https://t.me/get10101',
+              },
+              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/get10101/10101',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} 10101, Built with ♥️ by CoBloX`,
       },
       prism: {
         theme: lightCodeTheme,
