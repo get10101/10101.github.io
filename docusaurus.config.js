8eb61950e9a0a6a12bc4e86452cb3b7b495d7141
Daniel Karzel <daniel@comit.network>
2023-05-16 18:04:57 -0400

    Configure g-tag to track docusaurus pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-ZNPPSDPC4H",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
