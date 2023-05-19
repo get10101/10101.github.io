b1ba1642d74d6686f3cf9d3a6881962a94c67030
Daniel Karzel <D.Karzel@gmail.com>
2023-05-20 06:02:06 +1000

    Revert "Revert "Add viral loops link""

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 function HomePage() {
-  let frontpage = useBaseUrl("/frontpage/index.html");
+  let frontpage =
+    useBaseUrl("/frontpage/index.html");
 
   React.useEffect(() => {
+    let params = document.location.search;
     // Redirect to the static index.html
-    window.location.href = frontpage;
+    window.location.href = frontpage + params;
   }, []);
 
   // Render an empty component
